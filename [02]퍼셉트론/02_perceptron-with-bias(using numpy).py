adbf0501e49d25f96346490ce1f31d6740b4d291
PresentJay <presentj94@gmail.com>
2021-01-04 21:35:14 +0900

    [퍼셉트론] 편향 AND

---
+++ [02]퍼셉트론/02_perceptron-with-bias(using numpy).py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def Bias_Perceptron(arr_x):
+    weight = np.array([0.5, 0.5])
+    bias = -0.7
+    tmp = np.sum(weight*arr_x) + bias
+    
+    # print(tmp)
+    # tmp is -0.1999 ... (approximately -0.2) : calculation error caused by floating point
+    
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    x = np.array([0, 1])
+    print(Bias_Perceptron(x))
+    
