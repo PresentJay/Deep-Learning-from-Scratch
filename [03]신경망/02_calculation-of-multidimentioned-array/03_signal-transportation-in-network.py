a97519aa2d3399c0cc7581f854cfb93a83a18e51
PresentJay <presentj94@gmail.com>
2021-01-07 16:53:04 +0900

    project packaging & calculating network-signal-transportation using matrix-multiplication

---
+++ [03]신경망/02_calculation-of-multidimentioned-array/03_signal-transportation-in-network.py
@@ -0,0 +1,38 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+from functions import *
+import numpy as np
+
+def test():
+    # from input layer to first layer
+    X = np.array([1.0, 0.5])
+    W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    B1 = np.array([0.1, 0.2, 0.3])
+    
+    A1 = np.dot(X, W1) + B1
+    Z1 = sigmoid(A1)
+    
+    # print(A1)
+    # print(Z1)
+    
+    # from first layer to second layer
+    W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    B2 = np.array([0.1, 0.2])
+    
+    A2 = np.dot(Z1, W2) + B2
+    Z2 = sigmoid(A2)
+    
+    # print(A1)
+    # print(Z1)
+    
+    # from second layer to output layer
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    B3 = np.array([0.1, 0.2])
+    
+    A3 = np.dot(Z2, W3) + B3
+    
+    return identity_function(A3)
+    
+
+if __name__ == "__main__":
+    print(test())
---
+++ functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+# 항등함수
+def identity_function(x):
+    return x
