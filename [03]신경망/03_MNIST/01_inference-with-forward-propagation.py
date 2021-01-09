9ab2873d4e2a7dd07b1a58e3bc494e175f1c2498
PresentJay <presentj94@gmail.com>
2021-01-09 17:50:13 +0900

    mnist : calculating accuracy

--- [03]신경망/03_MNIST/01_inference-with-forward-propagation.py
+++ [03]신경망/03_MNIST/01_inference-with-forward-propagation.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, pickle
 from mnist import load_mnist
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 from functions import *
@@ -8,16 +8,54 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open(os.path.abspath(os.path.dirname(__file__)) + "/sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    
+    return softmax(a3)
 
 if __name__ == "__main__":
-    (x_train, t_train), (x_text, t_text) = load_mnist(flatten=True, normalize=False)
+    
+    x, t = get_data()
+    network = init_network()
+    
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
+        if p == t[i]:
+            accuracy_cnt += 1
+          
+    print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+    
+    
+    
+    
+    
+    
+    """ (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
     # flatten parameter makes input images simple array
     # normalize parameter normalizes input image's pixels to between 0.0 and 1.0 (or it can be 0~255)
     
     print(x_train.shape)
     print(t_train.shape)
-    print(x_text.shape)
-    print(t_text.shape)
+    print(x_test.shape)
+    print(t_test.shape)
     
     img = x_train[0]
     label = t_train[0]
@@ -27,4 +65,4 @@ if __name__ == "__main__":
     img = img.reshape(28, 28)
     print(img.shape)
     
-    img_show(img)
+    img_show(img) """
