1d4358bc21f9aa3039f3618b5ba9de242fd97a6a
PresentJay <presentj94@gmail.com>
2021-01-10 23:29:40 +0900

    편미분 계산

---
+++ [04]신경망학습/06_partial-diff.py
@@ -0,0 +1,28 @@
+import sys, os
+DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(DIR)
+from functions import *
+
+# 편미분 : 변수가 여럿인 함수의 미분
+
+def function(x):
+    return x[0]**2 + x[1]**2
+    # return np.sum(x**2)
+
+
+# 문제A : x0=3, x1=4일 때 x0에 대한 편미분을 구하여라
+# x0을 목표로, x1을 4.0으로 고정함
+def function_tmp1(x0):
+    return x0*x0 + 4.0**2.0
+
+# 문제B : x0=3, x1=4일 때 x1에 대한 편미분을 구하여라
+# x1을 목표로, x0을 3.0으로 고정함
+def function_tmp2(x1):
+    return 3.0**2.0 + x1*x1
+
+if __name__ == "__main__":
+    # 정답A
+    print(numerical_diff(function_tmp1, 3.0))
+    
+    # 정답B
+    print(numerical_diff(function_tmp2, 4.0))
--- functions.py
+++ functions.py
@@ -22,6 +22,18 @@ def softmax(x):
 def sum_squares_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
-def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t*np.log(y + delta))
+def cross_entropy_error(y, t, onehot=True):
+    # 데이터가 1차원일 경우
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    if onehot==True:
+        return -np.sum(t * np.log(y + 1e-7)) / batch_size
+    else:
+        return -np.sum(np.log(y[np.arange(batch_size), t]+ 1e-7)) / batch_size
+
+def numerical_diff(f, x):
+    h = 1e-4  # 0.0001 = (10 ^ (-4))
+    return (f(x+h) - f(x-h)) / (2*h)
